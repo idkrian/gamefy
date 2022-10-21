3998aae00f46965caf946403b56a35b7e3ca062b
Rian <riansouza0@gmail.com>
2022-10-21 00:06:59 -0300

    fix: jwt implementation

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module, Type } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core'
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard
+  }],
 })
-export class AppModule {}
+export class AppModule { }
