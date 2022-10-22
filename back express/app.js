9d84f23d179be260ae647f071a8a7832ff89e4a4
Rian <riansouza0@gmail.com>
2022-10-22 01:43:38 -0300

    fix: simple authentication fix

--- back express/app.js
+++ back express/app.js
@@ -15,13 +15,115 @@ app.get('/', (req, res) => {
 //Models
 const User = require('./models/User')
 
+//Private Route
+app.get('/user/:id', checkToken, async (req, res) => {
+    const { id } = req.params
+
+    const user = await User.findById(id, "-password")
+
+    if (!user) {
+        return res.status(422).json({ msg: "Usuário não encontrado" })
+    }
+
+    res.status(200).json({ user })
+
+})
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({ msg: "Acesso negado!" })
+
+    }
+
+    try {
+        const secret = process.env.JWT_SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch (error) {
+        res.status(400).json({ msg: "Token inválido!" })
+    }
+}
+
 //Registrar Usuário
 app.post('/auth/register', async (req, res) => {
 
     const { name, email, password, confirmpassword } = req.body
 
     if (!name) {
-        return res.status(422).json({ msg: "Nome é obrigatório" })
+        return res.status(422).json({ msg: "O Nome é obrigatório" })
+    }
+    if (!email) {
+        return res.status(422).json({ msg: "O email é obrigatório" })
+    }
+    if (!password) {
+        return res.status(422).json({ msg: "A Senha é obrigatória" })
+    }
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: "As Senhas não conferem" })
+    }
+
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+        return res.status(422).json({ msg: "Por favor utilize outro email" })
+    }
+
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const user = new User({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    try {
+        await user.save()
+        res.status(201).json({ msg: "Usuário criado com sucesso!" })
+
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+})
+
+//Login do Usuário
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body
+    if (!email) {
+        return res.status(422).json({ msg: "O email é obrigatório" })
+    }
+    if (!password) {
+        return res.status(422).json({ msg: "A Senha é obrigatória" })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        return res.status(422).json({ msg: "Usuário não encontrado!" })
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+        return res.status(422).json({ msg: "Senha inválida!" })
+    }
+
+    try {
+        const secret = process.env.JWT_SECRET
+
+        const token = jwt.sign({
+            id: user._id
+        }, secret)
+
+        res.status(200).json({ message: 'Autenticado com sucesso!', token })
+    } catch (error) {
+        res.status(500).json({ message: error })
+
     }
 
 })
